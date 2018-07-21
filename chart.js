a6120691565af136003de19dbd8f838e0c5a7db9
Tanakorn Prangsriarun <tanagon601@gmail.com>
2018-07-21 22:53:44 +0700

    page fix

--- chart.js
+++ chart.js
@@ -1,37 +1,54 @@
 let lineY = 0;
 let lineX = 1;
-
+let adver = 0;
+let count = 1;
 var i;
 
 
 window.onload = function () {
-    
+
     var chart = new CanvasJS.Chart("chartContainer", {
         animationEnabled: true,
         axisY: {
             title: "Units Sound",
-            valueFormatString: "#0.",
-                                    
-            suffix: "dB",
-            stripLines: [{
-                value: 436,
-                label: "Average"
-            }]
+            valueFormatString: "#0,,.",
         },
         data: [{
-            yValueFormatString: "#,### dB",
+            yValueFormatString: "#,###",
             xValueFormatString: "YYYY",
             type: "spline",
             dataPoints: [
-                {x: lineX, y: lineY},
+                {x: 1, y: 1}
 
             ]
         }]
     });
-    for (i = 0; i < 4; i++) { 
-        lineY = i
-        lineX = i
-        chart.options.data[0].dataPoints.push({x: lineX, y: lineY});
-    }
-    chart.render();
-    }
+
+    setInterval(function () {
+
+        $.ajax({
+            type: "GET",
+            url: "http://ecourse.cpe.ku.ac.th/exceed/api/wongpalm-Sound_level/view",
+            data: "data",
+            dataType: "text",
+            success: function (response) {
+                let a = parseInt(response)
+                console.log(response);
+                if(response !== '0' ){
+                    count = count +1;
+                    chart.options.data[0].dataPoints.push({ x: count, y: a });
+                    console.log(response);
+                    chart.render();
+                }
+            },
+            fall: function(response){
+                console.log(response)
+            }
+        });
+
+    }, 1000);
+
+
+
+
+}
